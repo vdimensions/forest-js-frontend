f40c841c9decf4149d4ffd8305c11dd10bc10de9
ivaylo5ev <ivaylo5ev@gmail.com>
2021-06-20 23:08:01 +0300

    - added noop client implementation

--- src/client.ts
+++ src/client.ts
@@ -16,3 +16,10 @@ export interface IForestClient {
     navigate: (template: string) => Promise<ForestResponse>,
     invokeCommand: (instanceId: string, command: string, arg?: any) => Promise<ForestResponse>
 }
+
+class NoopForestClient implements IForestClient {
+    navigate: (template: string) => Promise<ForestResponse> = () => Promise.reject();
+    invokeCommand: (instanceId: string, command: string, arg?: any) => Promise<ForestResponse> = () => Promise.reject();
+}
+
+export const NoopClient = new NoopForestClient();
