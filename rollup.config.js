ff5ebad39ce37ff1d665dc6df272d1e0ec38f953
ivaylo5ev <ivaylo5ev@gmail.com>
2022-01-21 18:58:50 +0200

    - added support for rollup.js

---
+++ rollup.config.js
@@ -0,0 +1,68 @@
+import peerDepsExternal from "rollup-plugin-peer-deps-external";
+import resolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+import typescript from "rollup-plugin-typescript2";
+import copy from "rollup-plugin-copy";
+import license from "rollup-plugin-license";
+import merge from 'deepmerge';
+import { createBasicConfig } from '@open-wc/building-rollup';
+
+const path = require('path');
+const packageJson = require("./package.json");
+
+export default merge(createBasicConfig(), {
+  input: "tmp/tsc/src/index.js",
+  output: [
+    {
+      file: `dist/${packageJson.main}`,
+      format: "cjs",
+      sourcemap: true
+    },
+    {
+      file: `dist/${packageJson.module}`,
+      format: "esm",
+      sourcemap: true
+    }
+  ],
+  plugins: [
+    peerDepsExternal(),
+    resolve(),
+    commonjs(),
+    typescript({ useTsconfigDeclarationDir: true }),
+    copy({
+      targets: [
+        {
+          src: "package.json",
+          dest: "dist"
+        }
+      ]
+    }),
+    license({
+      sourcemap: true,
+      cwd: __dirname,//'.', // Default is process.cwd()
+
+      banner: {
+        commentStyle: 'regular', // The default
+
+        content: {
+          file: path.join(__dirname, 'LICENSE'),
+          encoding: 'utf-8', // Default is utf-8
+        },
+
+        // // Optional, may be an object or a function returning an object.
+        // data() {
+        //   return {
+        //   };
+        // },
+      },
+
+      // thirdParty: {
+      //   includePrivate: false, // Default is false.
+      //   output: {
+      //     file: path.join(__dirname, 'dist', 'dependencies.txt'),
+      //     encoding: 'utf-8', // Default is utf-8.
+      //   },
+      // },
+    }),
+  ]
+});
